6277b17ce40d1a0e4bb9cf022ac0fc8d0864fa99
CHIRAG24CODES <98631995+CHIRAG24CODES@users.noreply.github.com>
2022-01-30 13:55:13 +0530

    Update and rename README.md to table_do_while.js

---
+++ table_do_while.js
@@ -0,0 +1,9 @@
+# PRINTING-TABLE-USING-JS-DO-WHILE-LOOP
+PRINTING TABLE USING DO WHILE LOOP
+
+console.log('table of 10')
+let multi_of_10  = 10;
+let multi2 = 1;
+do {
+    console.log(multi_of_10 * multi2++)
+} while (!(multi2 > 10)) 
